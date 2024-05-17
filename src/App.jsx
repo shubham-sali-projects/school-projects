371f7735cdf5dd2855d3b01a3de520d133853956
shubham sali <salishubham46@gmail.com>
2024-05-17 19:41:51 +0530

    app modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import About from "./components/About/About";
+import Campus from "./components/Campus/Campus";
+import Contact from "./components/Contact/Contact";
 import Header from "./components/Header/Header";
 import Nav from "./components/NAVBAR/Nav";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Title from "./components/Title/Title";
 import Student from "./components/student-department/Student";
 
 function App() {
@@ -9,7 +14,15 @@ function App() {
         <Nav />
         <Header />
         <div className="container">
+          <Title subtitle="Our School" title="what we offer" />
           <Student />
+          <About />
+          <Title subtitle="Gallery" title="campus photo" />
+          <Campus />
+          <Title subtitle="TESTIMONIALS" title="What Student Says" />
+          <Testimonials />
+          <Title subtitle="Contact Us" title="Get in Touch" />
+          <Contact />
         </div>
       </div>
     </>
